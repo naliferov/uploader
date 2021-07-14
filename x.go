af626dba0447a571c2fe5f02695b6dc669b01b45
any <x8core@gmail.com>
2021-07-14 17:23:18 +0300

    update

--- x.go
+++ x.go
@@ -2,34 +2,27 @@ package main
 
 import (
 	"fmt"
+	"io"
 	"net/http"
+	"os"
 )
 
 func uploadFile(w http.ResponseWriter, r *http.Request) {
 	// Maximum upload of 10 MB files
-	r.ParseMultipartForm(10 << 20)
+	r.ParseMultipartForm(2 << 20)
 
-	fhs := r.MultipartForm.File["myfiles"]
+	fhs := r.MultipartForm.File["uploads"]
 	for _, fh := range fhs {
-		f, err := fh.Open()
 
-		defer f.Close()
-		//fmt.Printf(f, err)
-		// f is one of the files
+		tempFile, _ := fh.Open()
+		newFile, err := os.OpenFile("./"+fh.Filename, os.O_WRONLY|os.O_CREATE, 0666)
+		if err != nil {
+			panic(err)
+		}
+		io.Copy(newFile, tempFile)
+		fmt.Printf("Uploaded File: %+v\n", fh.Filename)
+		fmt.Fprintf(w, "Uploaded File: %+v\n", fh.Filename)
 	}
-
-	// Get handler for filename, size and headers
-	file, handler, err := r.FormFile("myFile")
-	if err != nil {
-		fmt.Println("Error Retrieving the File")
-		fmt.Println(err)
-		return
-	}
-
-	defer file.Close()
-	fmt.Printf("Uploaded File: %+v\n", handler.Filename)
-	fmt.Printf("File Size: %+v\n", handler.Size)
-	fmt.Printf("MIME Header: %+v\n", handler.Header)
 }
 
 func display(w http.ResponseWriter, r *http.Request) {
